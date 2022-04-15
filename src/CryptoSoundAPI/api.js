21e720700774171073914c6193757d37ab2b8921
Christopher Smith <cryptochrisjames@Christophers-MacBook-Pro.local>
2022-04-15 12:09:57 -0500

    Added docker support for api.

---
+++ src/CryptoSoundAPI/api.js
@@ -0,0 +1,13 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+app.get('/', (req,res) => {
+    res.send("hElLo WOrLd");
+});
+
+if(process.env.NODE_ENV == 'prod') {
+
+} else {
+    app.listen(8100);
+    console.log('CS API is running.');
+}
