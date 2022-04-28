6cee601a1f9bed443afb62608fd012b4cf116e64
Christopher Smith <cryptochrisjames@Christophers-MacBook-Pro.local>
2022-04-28 02:22:55 -0500

    starting up with the infrastructure.

---
+++ tf/global/main.tf
@@ -0,0 +1,55 @@
+terraform {
+    backend "s3" {
+        bucket = "crypto-sound-terraform"
+        key = "cs-terraform-statefile.tfstate"
+        region = "us-east-1"
+    }
+    
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.27"
+        }
+    }
+
+    required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+    profile = "default"
+    region  = "us-east-1"
+}
+
+resource "aws_codebuild_project" "cs-terraform-global" {
+    name          = "cs-terraform-global"
+    description   = "CodeBuild project for building up the rest of the CICD pipeline"
+    build_timeout = "5"
+    service_role  = "arn:aws:iam::482352589093:role/service-role/codebuild-terraform-global-service-role"
+
+    artifacts {
+        type = "NO_ARTIFACTS"
+    }
+
+    environment {
+        compute_type                = "BUILD_GENERAL1_SMALL"
+        image                       = "aws/codebuild/standard:1.0"
+        type                        = "LINUX_CONTAINER"
+        image_pull_credentials_type = "CODEBUILD"
+    }
+
+    source {
+        type            = "GITHUB"
+        location        = "https://github.com/StoryCollectorIO/CryptoSound.git"
+        git_clone_depth = 1
+
+        git_submodules_config {
+            fetch_submodules = true
+        }
+    }
+
+    source_version = "master"
+
+    tags = {
+        Environment = "qa"
+    }
+}
