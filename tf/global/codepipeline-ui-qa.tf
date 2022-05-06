bee8b023f07766c716639155a342c9f1ac6b17d2
Christopher Smith <csmith4work@gmail.com>
2022-05-06 11:09:36 -0500

    Had to fix whitespace issues.

--- tf/global/codepipeline-ui-qa.tf
+++ tf/global/codepipeline-ui-qa.tf
@@ -45,62 +45,62 @@ resource "aws_codepipeline" "cs_pipeline_qa" {
 }
 
 resource "aws_iam_role" "codepipeline_role" {
-    name = "test-role"
+    name = "codepipeline-assume-role"
 
     assume_role_policy = <<EOF
-    {
+{
     "Version": "2012-10-17",
     "Statement": [
         {
-        "Effect": "Allow",
-        "Principal": {
-            "Service": "codepipeline.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "codepipeline.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
         }
     ]
-    }
-    EOF
-    }
+}
+EOF
+}
 
-    resource "aws_iam_role_policy" "codepipeline_policy" {
+resource "aws_iam_role_policy" "codepipeline_policy" {
     name = "codepipeline_policy"
     role = aws_iam_role.codepipeline_role.id
 
     policy = <<EOF
-    {
+{
     "Version": "2012-10-17",
     "Statement": [
         {
-        "Effect":"Allow",
-        "Action": [
-            "s3:GetObject",
-            "s3:GetObjectVersion",
-            "s3:GetBucketVersioning",
-            "s3:PutObjectAcl",
-            "s3:PutObject"
-        ],
-        "Resource": [
-            "${aws_s3_bucket.codepipeline_bucket.arn}",
-            "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
+            "Effect":"Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:GetBucketVersioning",
+                "s3:PutObjectAcl",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "${aws_s3_bucket.codepipeline_bucket.arn}",
+                "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+            ]
         },
         {
-        "Effect": "Allow",
-        "Action": [
-            "codestar-connections:UseConnection"
-        ],
-        "Resource": "${aws_codestarconnections_connection.codepipeline_connection.arn}"
+            "Effect": "Allow",
+            "Action": [
+                "codestar-connections:UseConnection"
+            ],
+            "Resource": "${aws_codestarconnections_connection.codepipeline_connection.arn}"
         },
         {
-        "Effect": "Allow",
-        "Action": [
-            "codebuild:BatchGetBuilds",
-            "codebuild:StartBuild"
-        ],
-        "Resource": "*"
+            "Effect": "Allow",
+            "Action": [
+                "codebuild:BatchGetBuilds",
+                "codebuild:StartBuild"
+            ],
+            "Resource": "*"
         }
     ]
-    }
-    EOF
+}
+EOF
 }
