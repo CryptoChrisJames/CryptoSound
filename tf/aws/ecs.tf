6d5c8b60314f3f37171a81a866a48cdae3212def
Christopher Smith <csmith4work@gmail.com>
2022-05-19 18:50:09 -0500

    Fixing ECS.

--- tf/aws/ecs.tf
+++ tf/aws/ecs.tf
@@ -47,11 +47,93 @@ resource "aws_ecs_service" "cs_ui_service" {
     network_configuration {
         subnets = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
         assign_public_ip = true
+        security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+    }
+    load_balancer {
+        target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+        container_name   = "${aws_ecs_task_definition.scp_cs_ui_task.family}"
+        container_port   = 80 # Specifying the container port
+    }
+}
+
+resource "aws_alb" "application_load_balancer" {
+    name               = "cs-alb" # Naming our load balancer
+    load_balancer_type = "application"
+    subnets = [ # Referencing the default subnets
+        "${aws_default_subnet.default_subnet_a.id}",
+        "${aws_default_subnet.default_subnet_b.id}"
+    ]
+    # Referencing the security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+    ingress {
+        from_port   = 80 # Allowing traffic in from port 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+    }
+
+    ingress {
+        from_port   = 443 # Allowing traffic in from port 80
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+    }
+
+    egress {
+        from_port   = 0 # Allowing any incoming port
+        to_port     = 0 # Allowing any outgoing port
+        protocol    = "-1" # Allowing any outgoing protocol
+        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+    }
+}
+
+resource "aws_security_group" "service_security_group" {
+    ingress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        # Only allowing traffic in from the load balancer security group
+        security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+    }
+
+    egress {
+        from_port   = 0 # Allowing any incoming port
+        to_port     = 0 # Allowing any outgoing port
+        protocol    = "-1" # Allowing any outgoing protocol
+        cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+    }
+}
+resource "aws_lb_target_group" "target_group" {
+    name        = "cs-target-group-${var.env}"
+    port        = 80
+    protocol    = "HTTP"
+    target_type = "ip"
+    vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+    health_check {
+        matcher = "200,301,302"
+        path = "/"
+    }
+}
+
+resource "aws_lb_listener" "listener" {
+    load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+    port              = "80"
+    protocol          = "HTTP"
+    default_action {
+        type             = "forward"
+        target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
     }
 }
 
 # Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" {
+    tags = {
+        Name = "Default VPC"
+    }
 }
 
 # Providing a reference to our default subnets
