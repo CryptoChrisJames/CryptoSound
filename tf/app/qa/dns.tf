ebede465d3d12903a4663039617103d4cf67f6eb
Christopher Smith <csmith4work@gmail.com>
2022-06-14 10:54:35 -0500

    will worry about site rules later

--- tf/app/qa/dns.tf
+++ tf/app/qa/dns.tf
@@ -28,17 +28,17 @@ resource "cloudflare_record" "cs_api_cname" {
     proxied = true
 }
 
-resource "cloudflare_page_rule" "https" {
-    zone_id = data.cloudflare_zones.cs_domain.zones[0].id
-    target  = "*.${var.site_domain}/*"
-    priority = 2
-    lifecycle {
-        ignore_changes = [priority]
-    }
-    actions {
-        always_use_https = true
-    }
-}
+# resource "cloudflare_page_rule" "https" {
+#     zone_id = data.cloudflare_zones.cs_domain.zones[0].id
+#     target  = "*.${var.site_domain}/*"
+#     priority = 2
+#     lifecycle {
+#         ignore_changes = [priority]
+#     }
+#     actions {
+#         always_use_https = true
+#     }
+# }
 
 data "cloudflare_zones" "cs_domain" {
     filter {
