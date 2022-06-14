cf22ba6a511c0892fe66914f031abe761a8968d2
Christopher Smith <csmith4work@gmail.com>
2022-06-14 10:41:10 -0500

    Hopefully fixing issue with resources I forgot to delete.

--- tf/app/qa/dns.tf
+++ tf/app/qa/dns.tf
@@ -31,6 +31,10 @@ resource "cloudflare_record" "cs_api_cname" {
 resource "cloudflare_page_rule" "https" {
     zone_id = data.cloudflare_zones.cs_domain.zones[0].id
     target  = "*.${var.site_domain}/*"
+    priority = 2
+    lifecycle {
+        ignore_changes = [priority]
+    }
     actions {
         always_use_https = true
     }
