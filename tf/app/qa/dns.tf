9c270442a557099999df04666c8ad650ed810e9c
Christopher Smith <csmith4work@gmail.com>
2022-06-21 18:47:46 -0500

    Setting up DNS for ui.

--- tf/app/qa/dns.tf
+++ tf/app/qa/dns.tf
@@ -1,22 +1,12 @@
-# resource "cloudflare_record" "cs_prod_ui_cname" {
-#     zone_id = data.cloudflare_zones.domain.zones[0].id
-#     name    = var.site_domain
-#     value   = aws_s3_bucket.site.website_endpoint
-#     type    = "CNAME"
+resource "cloudflare_record" "cs_ui_cname" {
+    zone_id = data.cloudflare_zones.cs_domain.zones[0].id
+    name    = var.env
+    value   = module.s3.ui_endpoint
+    type    = "CNAME"
 
-#     ttl     = 1
-#     proxied = true
-# }
-
-# resource "cloudflare_record" "cs_prod_ui_www" {
-#     zone_id = data.cloudflare_zones.domain.zones[0].id
-#     name    = "www"
-#     value   = var.site_domain
-#     type    = "CNAME"
-
-#     ttl     = 1
-#     proxied = true
-# }
+    ttl     = 1
+    proxied = true
+}
 
 resource "cloudflare_record" "cs_api_cname" {
     zone_id = data.cloudflare_zones.cs_domain.zones[0].id
@@ -27,18 +17,6 @@ resource "cloudflare_record" "cs_api_cname" {
     ttl     = 1
     proxied = true
 }
-
-# resource "cloudflare_page_rule" "https" {
-#     zone_id = data.cloudflare_zones.cs_domain.zones[0].id
-#     target  = "*.${var.site_domain}/*"
-#     priority = 2
-#     lifecycle {
-#         ignore_changes = [priority]
-#     }
-#     actions {
-#         always_use_https = true
-#     }
-# }
 
 data "cloudflare_zones" "cs_domain" {
     filter {
