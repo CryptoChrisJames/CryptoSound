7f0557b663a5758d59f3e684cdeec118b2775f56
Christopher Smith <csmith4work@gmail.com>
2022-06-21 16:55:45 -0500

    adding website hosting to s3

--- tf/modules/s3/main.tf
+++ tf/modules/s3/main.tf
@@ -6,3 +6,15 @@ resource "aws_s3_bucket_acl" "ui_build_bucket_acl" {
     bucket = aws_s3_bucket.ui_build_bucket.id
     acl    = "private"
 }
+
+resource "aws_s3_bucket_website_configuration" "example" {
+    bucket = aws_s3_bucket.ui_build_bucket.bucket
+
+    index_document {
+        suffix = "index.html"
+    }
+
+    error_document {
+        key = "error.html"
+    }
+}
